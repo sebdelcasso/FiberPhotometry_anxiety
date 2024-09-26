b1de28a70dedc2615a7d6b9b2384cf91e2ddc153
sebdelcasso <159019540+sebdelcasso@users.noreply.github.com>
2024-09-26 18:05:24 +0200

    Update import_kdenlive_xml.m

--- behavior_tools/import_kdenlive_xml.m
+++ behavior_tools/import_kdenlive_xml.m
@@ -1,11 +1,17 @@
 function import_kdenlive_xml(filename)
 [filepath,name,ext] = fileparts(filename);
 s = xml2struct(filename);
-txt_tmp = s.mlt.chain.property{42}.Text;
+chains =  s.mlt.chain;
+chain_id = size(chains,2)
+if chain_id>1
+    chain = chains{1,chain_id}
+else
+    chain = chains
+end
+txt_tmp = chain.property{42}.Text;
 json_decoded = jsondecode(txt_tmp);
 fod = fopen([filepath filesep name '_events.txt'],'w');
 for i=1:size(json_decoded,1)
     fprintf(fod,"%d\n",json_decoded(i).pos);
 end
 fclose(fod);
-    
