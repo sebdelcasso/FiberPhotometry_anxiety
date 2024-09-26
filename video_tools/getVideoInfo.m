e3d7a0021424d8386e0eb41aeee7517dcee78f4e
sebdelcasso <159019540+sebdelcasso@users.noreply.github.com>
2024-09-26 18:07:00 +0200

    Update getVideoInfo.m

--- video_tools/getVideoInfo.m
+++ video_tools/getVideoInfo.m
@@ -1,13 +1,24 @@
+% function videoInfo=getVideoInfo(params)
+%     videoInfoPath = [params.dataRoot  filesep params.dataFileTag '-videoInfo.mat'];
+%     if ~exist(videoInfoPath,'file')
+%         videoPath = [params.dataRoot filesep params.dataFileTag '.' params.videoExtension];
+%         videoInfo = VideoReader(videoPath);
+%         save(videoInfoPath,'videoInfo');
+%     end
+%      load(videoInfoPath)
+% end
+
 function videoInfo=getVideoInfo(params)
     videoInfoPath = [params.dataRoot  filesep params.dataFileTag '-videoInfo.mat'];
     if ~exist(videoInfoPath,'file')
         videoPath = [params.dataRoot filesep params.dataFileTag '.' params.videoExtension];
-        videoInfo = VideoReader(videoPath);
+        tmp = VideoReader(videoPath);
+        videoInfo = struct();
+        videoInfo.FrameRate = tmp.FrameRate;
         save(videoInfoPath,'videoInfo');
     else
         try
-            load(videoInfoPath)
-%             eval('videoInfo.FrameRate');
+            load(videoInfoPath);
         catch
             delete(videoInfoPath);
             videoInfo=getVideoInfo(params);
